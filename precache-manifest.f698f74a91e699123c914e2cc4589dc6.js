13d2c686aa6cb13fd75b2c13fe631031372059c9
Connor Twele <connortwele@gmail.com>
2020-08-07 09:24:37 -0400

    Updates

---
+++ precache-manifest.f698f74a91e699123c914e2cc4589dc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f46542f32de7f08692332e3ca4c21c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b723984775fa9eec5dce",
+    "url": "/static/css/main.8fe48eb4.chunk.css"
+  },
+  {
+    "revision": "51b4fd1e8de726ae5b80",
+    "url": "/static/js/2.9cecdd42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9cecdd42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b723984775fa9eec5dce",
+    "url": "/static/js/main.2a1a4f08.chunk.js"
+  },
+  {
+    "revision": "a286160a91738237962a",
+    "url": "/static/js/runtime-main.8d62b331.js"
+  },
+  {
+    "revision": "29fedb34adc53dfb1fde5d35929d0126",
+    "url": "/static/media/cap.29fedb34.png"
+  },
+  {
+    "revision": "0724cd6b2efba09b74cbf1cc0a874127",
+    "url": "/static/media/rootbeer.0724cd6b.png"
+  }
+]);
